a2945e89ac0f30c357434fce33c524fd56d20a55
Wesley Tiozzo <tiozzo.wesley1@gmail.com>
2022-01-18 16:17:11 -0300

    14 - Removed games.log

--- spec/lib/parse_spec.rb
+++ spec/lib/parse_spec.rb
@@ -1,26 +1,28 @@
 require_relative '../spec_helper'
 require_relative '../../lib/parse'
 
-describe '#fopen' do
-    context "when the file exists:" do
-        it 'open the file' do
-            #Input
-            parse = Parse.new("../../log/games.log")
-            parse.fopen
+describe Parse do
+    describe '#fopen' do
+        context "when the file exists:" do
+            it 'open the file' do
+                #Input
+                parse = Parse.new("../../log/games.log")
+                parse.fopen
 
-            #Output
-            expect(parse.get_line()).to eq("  0:00 ------------------------------------------------------------")
+                #Output
+                expect(parse.get_line()).to eq("  0:00 ------------------------------------------------------------")
+            end
         end
-    end
 
-    context "when the file doesn't exist:" do
-        it "return an error message" do
-            #Input
-            parse = Parse.new("invalid-path")
-            parse.fopen
+        context "when the file doesn't exist:" do
+            it "return an error message" do
+                #Input
+                parse = Parse.new("invalid-path")
+                parse.fopen
 
-            #Output
-            expect(parse.fopen()).to eq(nil)
+                #Output
+                expect(parse.fopen()).to eq(nil)
+            end
         end
     end
 end
