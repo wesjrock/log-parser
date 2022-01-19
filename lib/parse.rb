9d19a3f3483d6fcd5c72835418b765f8b9cc73b6
Wesley Tiozzo <tiozzo.wesley1@gmail.com>
2022-01-19 17:03:28 -0300

    18 - Indented Code

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,41 +1,41 @@
 require 'json'
 
 class Parse
-   def initialize(path)
-      @path = path
-      @lines = []
-   end
+  def initialize(path)
+    @path = path
+    @lines = []
+  end
 
-   def fopen
-      if File.exists?(@path)
-         file = File.open(@path)
-         begin
-            @lines = file.readlines.map(&:chomp)
-         rescue
-            return nil
-         ensure
-            file.close
-         end
+  def fopen
+    if File.exists?(@path)
+      file = File.open(@path)
+      begin
+        @lines = file.readlines.map(&:chomp)
+      rescue
+        return nil
+      ensure
+        file.close
       end
-   end
+    end
+  end
 
-   def get_first_line
-      return @lines[0]
-   end
+  def get_first_line
+    return @lines[0]
+  end
 
-   def convert_to_json
-      my_object = {
-         "#{@path}": {
-         lines: count_lines()
-         } 
-      }
-      json_object = my_object.to_json
-   end
+  def output
+    my_object = {
+      "#{@path}": {
+        lines: count_lines()
+      } 
+    }
+    json_object = my_object.to_json
+  end
 
-   private
+  private
 
-   def count_lines
-      @lines.size
-   end   
+  def count_lines
+    @lines.size
+  end   
 
 end
--- spec/lib/parse_spec.rb
+++ spec/lib/parse_spec.rb
@@ -2,38 +2,38 @@ require_relative '../spec_helper'
 require_relative '../../lib/parse'
 
 describe Parse do
-        describe '#fopen' do
-            context "when the file exists:" do
-                it 'open the file' do
-                    parse = Parse.new("../../log/games.log")
-                    parse.fopen
+  describe '#fopen' do
+    context "when the file exists:" do
+      it 'open the file' do
+        parse = Parse.new("../../log/games.log")
+        parse.fopen
 
-                    expect(parse.get_first_line()).to eq("  0:00 ------------------------------------------------------------")
-                end
-            end
+        expect(parse.get_first_line()).to eq("  0:00 ------------------------------------------------------------")
+      end
+    end
 
-            context "when the file doesn't exist:" do
-                it "return an error message" do
-                    parse = Parse.new("invalid-path")
-                    parse.fopen
+    context "when the file doesn't exist:" do
+      it "return an error message" do
+        parse = Parse.new("invalid-path")
+        parse.fopen
 
-                    expect(parse.fopen()).to eq(nil)
-                end
-            end
-        end
+        expect(parse.fopen()).to eq(nil)
+      end
+    end
     
-        describe '#convert_to_json' do
-            context "when counting line numbers:" do
-                it "returns a JSON object" do
-                    parse = Parse.new("../../log/games.log")
-                    parse.fopen
-                    my_json_object = parse.convert_to_json
-                    expected_json_object = {"../../log/games.log":{"lines":5306}}.to_json
+  end
 
-                    expect(my_json_object).to eq(expected_json_object)
-                end
-            end
-        end
-    
+  describe '#convert_to_json' do
+    context "when counting line numbers:" do
+      it "returns a JSON object" do
+        parse = Parse.new("../../log/games.log")
+        parse.fopen
+        my_json_object = parse.output
+        expected_json_object = {"../../log/games.log":{"lines":5306}}.to_json
+
+        expect(my_json_object).to eq(expected_json_object)
+      end
+    end
+  end
+
 end
-
